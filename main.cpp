788b8887e431d75938c3ad374e20b41bfc623dcd
yrst <urryst@gmail.com>
2019-03-10 15:49:22 +0700

    Fixed issue with type of output variables

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  /* code */
+  char pole [9][9]={
+    {'8','r','n','b','q','k','b','n','r'},
+    {'7','p','p','p','p','p','p','p','p'},
+    {'6',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'4',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'4',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'3',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'2','P','P','P','P','P','P','P','P'},
+    {'1','R','N','B','Q','K','B','N','R'},
+    {' ','a','b','c','d','e','f','g','h'}
+  };
+  for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+          cout << setw(2) << pole[i][j];
+      }
+    cout << endl;
+  }
+  return 0;
+}
