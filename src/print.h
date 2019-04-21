a8a71eac968529568facb36f6c01dd6d4a720719
yrst <urryst@gmail.com>
2019-04-21 18:34:38 +0700

    Create print.h library

---
+++ src/print.h
@@ -0,0 +1,14 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <iomanip>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+void ChessField(char pole[][9]);
+
+#endif
