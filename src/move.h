d9888bcb16f3eb0c0b9c690ca77dcd37fe3b5fc4
yrst <urryst@gmail.com>
2019-04-21 18:31:28 +0700

    Create move.h library with calling Move function

---
+++ src/move.h
@@ -0,0 +1,14 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+#include <iomanip>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+void Move(char pole[][9]);
+
+#endif
