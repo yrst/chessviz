3c4fe485b4893cee21237bdc3b59f25056c65c9d
yrst <urryst@gmail.com>
2019-05-06 10:17:22 +0700

    Upload for callin new functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
@@ -12,15 +13,18 @@ int main()
     char pole[9][9] = {{'8', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                        {'7', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                        {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                       {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                       {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                        {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                        {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                        {'2', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                        {'1', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
                        {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
+    int scorewhite = 0, scoreblack = 0, team = 0;
+    char Maswhite[16], Masblack[16];
+    ChessField(pole);
     while (true) {
+        Move(pole, &team, &scorewhite, &scoreblack, Maswhite, Masblack);
         ChessField(pole);
-        Move(pole);
     }
     return 0;
 }
