1f11f00405554f8fbcbde9e3c4f6dd1c9c9b6a25
yrst <urryst@gmail.com>
2019-05-20 10:01:46 +0700

    Add small fixes

--- test/main.cpp
+++ test/main.cpp
@@ -25,73 +25,73 @@ int main(int argc, const char** argv)
     lasttest = test1();
     res += !lasttest;
     fprintf(stderr,
-            "1 Проверка на принадлежность фигуры команде : %s\n",
+            "1 Проверка на принадлежность фигуры стороне, возможность хода "
+            "этой фигурой, в следствии вывод возможности хода в зависимости от "
+            "стороны : %s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test2();
     res += !lasttest;
     fprintf(stderr,
-            "2 Проверка на принадлежность фигуры команде : %s\n",
+            "2 Проверка на принадлежность фигуры стороне, возможность хода "
+            "этой фигурой, в следствии вывод возможности хода в зависимости от "
+            "стороны (2): %s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test3();
     res += !lasttest;
     fprintf(stderr,
-            "3 Проверка на принадлежность фигуры команде : %s\n",
+            "3 Проверка на принадлежность фигуры стороне, возможность хода "
+            "этой фигурой, в следствии вывод возможности хода в зависимости от "
+            "стороны (3): %s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test4();
     res += !lasttest;
     fprintf(stderr,
-            "4 Проверка на принадлежность фигуры команде : %s\n",
+            "4 Проверка на принадлежность фигуры стороне, возможность хода "
+            "этой фигурой, в следствии вывод возможности хода в зависимости от "
+            "стороны (4) : %s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test5();
     res += !lasttest;
     fprintf(stderr,
-            " 5 Проверка на возможность хода e2-a7 : %s\n",
+            " 5 Проверка на возможность хода фигурой по координатам e2-a7 : "
+            "%s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test6();
     res += !lasttest;
     fprintf(stderr,
-            "6 Проверка на возможность хода a2-a3: %s\n",
+            "6 Проверка на возможность хода фигурой по координатам  a2-a3: "
+            "%s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test7();
     res += !lasttest;
     fprintf(stderr,
-            "7 Проверка на возможнось хода a6-a7: %s\n",
+            "7 Проверка на возможнось хода фигурой по координатам  a6-a7: %s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test8();
     res += !lasttest;
     fprintf(stderr,
-            "8 Проверка на возмжность хода a1-a2: %s\n",
+            "8 Проверка на возмжность хода фигурой по координатам  a1-a2: %s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test9();
     res += !lasttest;
     fprintf(stderr,
-            "9 Проверка на возможность хода b1-c3: %s\n",
+            "9 Проверка на возможность хода фигурой по координатам  b1-c3: "
+            "%s\n",
             lasttest ? "TRUE" : "FALSE");
 
     lasttest = test10();
     res += !lasttest;
     fprintf(stderr,
-            "10 Проверка на возможность хода c1-a3: %s\n",
-            lasttest ? "TRUE" : "FALSE");
-
-    lasttest = test11();
-    res += !lasttest;
-    fprintf(stderr,
-            "11 Проверка на возможность хода d1-d2: %s\n",
-            lasttest ? "TRUE" : "FALSE");
-
-    lasttest = test12();
-    res += !lasttest;
-    fprintf(stderr,
-            "12 Проверка на возможность хода e1-e2: %s\n",
+            "10 Проверка на возможность хода фигурой по координатам  c1-a3: "
+            "%s\n",
             lasttest ? "TRUE" : "FALSE");
 
     return res;
